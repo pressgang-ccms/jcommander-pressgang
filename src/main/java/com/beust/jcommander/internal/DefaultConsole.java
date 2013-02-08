c05df61ba68d5280eee0f4523876dfa76f44e2d3
Lee Newson <lnewson@redhat.com>
2013-02-08 10:14:52 +1000

    Fixed an issue with reading content from stdin.

--- src/main/java/com/beust/jcommander/internal/DefaultConsole.java
+++ src/main/java/com/beust/jcommander/internal/DefaultConsole.java
@@ -1,43 +1,40 @@
 package com.beust.jcommander.internal;
 
-import com.beust.jcommander.ParameterException;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import com.beust.jcommander.ParameterException;
+
 public class DefaultConsole implements Console {
 
-  public void print(String msg) {
-    System.out.print(msg);
-  }
+    private final InputStreamReader isr = new InputStreamReader(System.in);
+    private final BufferedReader in = new BufferedReader(isr);
 
-  public void println(String msg) {
-    System.out.println(msg);
-  }
+    public void print(String msg) {
+        System.out.print(msg);
+    }
 
-  public char[] readPassword() {
-    try {
-      InputStreamReader isr = new InputStreamReader(System.in);
-      BufferedReader in = new BufferedReader(isr);
-      String result = in.readLine();
-      return result.toCharArray();
+    public void println(String msg) {
+        System.out.println(msg);
     }
-    catch (IOException e) {
-      throw new ParameterException(e);
+
+    public char[] readPassword() {
+        try {
+            String result = in.readLine();
+            return result.toCharArray();
+        } catch (IOException e) {
+            throw new ParameterException(e);
+        }
     }
-  }
-  
-  public String readLine() {
-    try {
-      InputStreamReader isr = new InputStreamReader(System.in);
-      BufferedReader in = new BufferedReader(isr);
-      String result = in.readLine();
-      return result;
+
+    public String readLine() {
+        try {
+            String result = in.readLine();
+            return result;
+        } catch (IOException e) {
+            throw new ParameterException(e);
+        }
     }
-    catch (IOException e) {
-      throw new ParameterException(e);
-    }
-  }
 
 }
